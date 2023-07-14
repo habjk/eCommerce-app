833aae6c89a26c0e4b84d2807bfe9b9c2fa4b12b
habjk <habibjmk@gmail.com>
2023-07-14 10:34:23 +0300

    poduct detail

---
+++ lib/screens/detail/detail_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shopping_app/constants.dart';
+import 'package:shopping_app/models/product.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Product product;
+  const DetailScreen({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: product.color,
+      appBar: AppBar(
+        backgroundColor: product.color,
+        elevation: 0,
+        foregroundColor: Colors.white,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            color: Colors.white,
+            icon: SvgPicture.asset(
+              backIcon,
+              width: 20,
+              color: Colors.white,
+            )),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              color: Colors.white,
+              icon: SvgPicture.asset(
+                searchIcon,
+                width: 20,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {},
+              color: Colors.white,
+              icon: SvgPicture.asset(
+                cartIcon,
+                width: 20,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(top: 5),
+          height: size.height,
+          child: Stack(
+            children: [
+              //Stack/white field
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: kDefaultPadding),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(product.title,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 11)),
+                          Text(product.title,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25)),
+                          Spacer(),
+                          Text("Price",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 11)),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 25),
+                            child: Text("\$${product.price}",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25)),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  //Spacer(),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(23),
+                            topRight: Radius.circular(23),
+                          )),
+                    ),
+                  )
+                ],
+              ),
+              //Stack/Image
+              Container(
+                  alignment: Alignment.centerRight,
+                  margin: EdgeInsets.only(bottom: size.height * .4, right: 20),
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxHeight: 175, maxWidth: 200),
+                    child: Image.asset(
+                      product.image,
+                      fit: BoxFit.fill,
+                      //height: 200,
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
